55a50f706a638a364f6fb575d9734aa980be3f7b
Brian Gillingham <bgilling@pitt.edu>
2018-01-11 09:38:23 -0500

    updated template to call the wrapping div book-viewer so that the islandora_internet_archive_bookreader will work

--- theme/islandora-manuscript-manuscript.tpl.php
+++ theme/islandora-manuscript-manuscript.tpl.php
@@ -6,7 +6,7 @@
 ?>
 <p class="subtitle"><?php print $upitt_viewer_link; ?></p>
 <?php if (isset($viewer)): ?>
-  <div id="manuscript-viewer">
+  <div id="book-viewer">
     <?php print $viewer; ?>
   </div>
 <?php endif; ?>
